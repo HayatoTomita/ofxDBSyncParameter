eb3d832560a58303fb17a3611c21ff1c1ec4c6d5
TOMITAHAYATO <nice.to.tom@gmail.com>
2020-12-06 23:00:50 +0900

    guiParameterの保存機能作ったのだけコミットしたかったけど気まぐれにclang-formatしてしまって無理になったので全部そのままコミットします

--- src/guiParameter.hpp
+++ src/guiParameter.hpp
@@ -8,17 +8,50 @@
 #ifndef guiParameter_hpp
 #define guiParameter_hpp
 
+#include "hiredis.h"
+#include <iostream>
 #include <stdio.h>
+#include <string>
 
-class guiParameter{
+class guiParameter {
 public:
-    guiParameter() {}
-    ~guiParameter() {}
-    void setValue(float _value) { value = _value; }
-    float getValue() { return value; }
-    float *getValuePtr() { return &value; }
+  guiParameter() {}
+  guiParameter(redisContext *_conn, std::string _key) {
+    key = _key;
+    conn = _conn;
+    resp = (redisReply *)redisCommand(conn, "GET %s", key.c_str());
+    if (NULL == resp) {
+      return false;
+    }
+    if (REDIS_REPLY_ERROR == resp->type) {
+      return false;
+    }
+    if (resp->str == NULL) {
+      std::cout << "init value" << std::endl;
+      dbsync();
+    }
+    value = std::atof(resp->str);
+  }
+  ~guiParameter() {}
+  void setValue(float _value) { value = _value; }
+  float getValue() { return value; }
+  float *getValuePtr() { return &value; }
+  bool dbsync() {
+    resp = (redisReply *)redisCommand(conn, "SET %s %f", key.c_str(), value);
+    if (NULL == resp) {
+      std::cout << "failed to set value." << std::endl;
+      return false;
+    }
+    if (REDIS_REPLY_ERROR == resp->type) {
+      return false;
+    }
+  }
+
 private:
-    float value;
+  std::string key;
+  float value = 0;
+  redisContext *conn = NULL;
+  redisReply *resp = NULL;
 };
 
 #endif /* guiParameter_hpp */
--- src/ofApp.cpp
+++ src/ofApp.cpp
@@ -1,99 +1,69 @@
 #include "ofApp.h"
 
 //--------------------------------------------------------------
-void ofApp::setup(){
-    gui.setup();
-    redisContext *conn = NULL;
-    redisReply *resp   = NULL;
-    conn = redisConnect( "127.0.0.1" , 6379);
-    if( ( NULL != conn ) && conn->err ){
-      printf("error : %s\n" , conn->errstr );
-      redisFree( conn );
-        return;
-    }else if( NULL == conn ){
-        return;
-    }
-    resp = (redisReply *) redisCommand( conn, "GET sample");
-    if( NULL == resp ){
-        redisFree( conn );
-        return;
-    }
-    if( REDIS_REPLY_ERROR == resp->type ){
-        redisFree( conn );
-        freeReplyObject( resp );
-        return;
-    }
-    printf( "sample : %s\n" , resp->str );
-    freeReplyObject( resp );
-    redisFree( conn );
-    
-    radius.setValue(0.0);
+void ofApp::setup() {
+  gui.setup();
+  redisContext *conn = NULL;
+  redisReply *resp = NULL;
+  conn = redisConnect("127.0.0.1", 6379);
+  if ((NULL != conn) && conn->err) {
+    printf("error : %s\n", conn->errstr);
+    redisFree(conn);
+    return;
+  } else if (NULL == conn) {
+    return;
+  }
+  radius = guiParameter(conn, "radius");
 }
 
 //--------------------------------------------------------------
-void ofApp::update(){
+void ofApp::update() {}
 
+//--------------------------------------------------------------
+void ofApp::draw() {
+  ofDrawCircle(ofGetWidth() / 2, ofGetHeight() / 2, radius.getValue());
+  gui.begin();
+  if (ImGui::SliderFloat("Float", radius.getValuePtr(), 0.0f, 100.0f)) {
+    radius.dbsync();
+  }
+  gui.end();
 }
 
 //--------------------------------------------------------------
-void ofApp::draw(){
-    ofDrawCircle(ofGetWidth() / 2, ofGetHeight() / 2, radius.getValue());
-    gui.begin();
-    ImGui::SliderFloat("Float", radius.getValuePtr(), 0.0f, 100.0f);
-    gui.end();
+void ofApp::exit() {
+  freeReplyObject(resp);
+  redisFree(conn);
 }
 
 //--------------------------------------------------------------
-void ofApp::keyPressed(int key){
-
-}
+void ofApp::keyPressed(int key) {}
 
 //--------------------------------------------------------------
-void ofApp::keyReleased(int key){
-
-}
+void ofApp::keyReleased(int key) {}
 
 //--------------------------------------------------------------
-void ofApp::mouseMoved(int x, int y ){
-
-}
+void ofApp::mouseMoved(int x, int y) {}
 
 //--------------------------------------------------------------
-void ofApp::mouseDragged(int x, int y, int button){
-
-}
+void ofApp::mouseDragged(int x, int y, int button) {}
 
 //--------------------------------------------------------------
-void ofApp::mousePressed(int x, int y, int button){
-
-}
+void ofApp::mousePressed(int x, int y, int button) {}
 
 //--------------------------------------------------------------
-void ofApp::mouseReleased(int x, int y, int button){
-
-}
+void ofApp::mouseReleased(int x, int y, int button) {}
 
 //--------------------------------------------------------------
-void ofApp::mouseEntered(int x, int y){
-
-}
+void ofApp::mouseEntered(int x, int y) {}
 
 //--------------------------------------------------------------
-void ofApp::mouseExited(int x, int y){
-
-}
+void ofApp::mouseExited(int x, int y) {}
 
 //--------------------------------------------------------------
-void ofApp::windowResized(int w, int h){
-
-}
+void ofApp::windowResized(int w, int h) {}
 
 //--------------------------------------------------------------
-void ofApp::gotMessage(ofMessage msg){
-
-}
+void ofApp::gotMessage(ofMessage msg) {}
 
 //--------------------------------------------------------------
-void ofApp::dragEvent(ofDragInfo dragInfo){ 
-
-}
+void ofApp::dragEvent(ofDragInfo dragInfo) {}
