dbaa7765a481fd86a205385a6821b3c9fdfc2c62
HayatoTomita <nice.to.tom@gmail.com>
2020-12-08 21:57:17 +0900

    redisからのreplyをパースしてunpackするまでをまとめた

--- src/guiParameter.hpp
+++ src/guiParameter.hpp
@@ -20,26 +20,21 @@ public:
   guiParameter(redisContext *_conn, std::string _key) {
     key = _key;
     conn = _conn;
-
     resp = (redisReply *)redisCommand(conn, "HGETALL %s", key.c_str());
     if (resp->element == NULL) {
       std::cout << "init value" << std::endl;
       value = T(0);
       dbsync();
-      resp = (redisReply *)redisCommand(conn, "HGETALL %s", key.c_str());
+      return;
     }
-    size_t data_size = std::atoi(resp->element[1]->str);
-    char *pdata = resp->element[3]->str;
-
-    msgpack::object_handle hd = msgpack::unpack(pdata, data_size);
-    const msgpack::object &obj = hd.get();
-    obj.convert(value);
+    unpackMessage(resp);
   }
-
   ~guiParameter() {}
+
   void setValue(T _value) { value = _value; }
   T getValue() { return value; }
   T *getValuePtr() { return &value; }
+
   bool dbsync() {
     msgpack::sbuffer buffer;
     msgpack::pack(buffer, value);
@@ -60,6 +55,13 @@ private:
   T value;
   redisContext *conn = NULL;
   redisReply *resp = NULL;
+  void unpackMessage(redisReply *resp) {
+    size_t data_size = std::atoi(resp->element[1]->str);
+    char *pdata = resp->element[3]->str;
+    msgpack::object_handle hd = msgpack::unpack(pdata, data_size);
+    const msgpack::object &obj = hd.get();
+    obj.convert(value);
+  }
 };
 
 #endif /* guiParameter_hpp */
